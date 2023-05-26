60d619078c7c1b95b20313caa9c985db35002153
ShevaMed <shevalenko87@gmail.com>
2023-05-26 17:40:26 +0300

    add init_database.sql

---
+++ init_database.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE IF NOT EXISTS doc_manage;
+Use doc_manage;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    user_password VARCHAR(30) NOT NULL, 
+    job_title VARCHAR(100) NOT NULL, 
+    user_type VARCHAR(30) NOT NULL,
+    signature LONGBLOB
+);
+
+CREATE TABLE IF NOT EXISTS documents (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    document_name VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    download_date DATE NOT NULL,
+    content LONGBLOB NOT NULL,
+    note TEXT,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+); 
+
+CREATE TABLE IF NOT EXISTS document_user (
+  document_id INT NOT NULL,
+  user_id INT NOT NULL,
+  check_signature BOOL NOT NULL,
+  user_note VARCHAR(255),
+  PRIMARY KEY (document_id, user_id),
+  FOREIGN KEY (document_id) REFERENCES documents(id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE settings (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name_ VARCHAR(100) NOT NULL UNIQUE,
+  value_ VARCHAR(255) NOT NULL
+);
+
